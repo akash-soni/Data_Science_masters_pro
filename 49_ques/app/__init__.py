ce78dbed9288d3c2b86226917cda04ebda140cc9
akash-soni <akash.200287@gmail.com>
2024-07-22 00:04:50 +0530

    assignment done

--- 49_ques/app/__init__.py
+++ 49_ques/app/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://akash200287:akash200287@cluster0.4bhfygp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+app.config['MONGO_URI'] = 'mongodb+srv://akash200287:<>@cluster0.4bhfygp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 app.config['SECRET_KEY'] = 'hidden'
 
 mongo = PyMongo(app)
